c667fd305a95e0af564a39d6c7bebb475a9064df
Rizky Benggolo <travarilo@gmail.com>
2018-10-15 07:42:27 +0000

    sdm660-common: rootdir: Add Spectrum support
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -389,7 +389,8 @@ PRODUCT_PACKAGES += \
     init.qcom.post_boot.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
-    init.zenparts.sh
+    init.zenparts.sh \
+    init.spectrum.sh
 
 PRODUCT_PACKAGES += \
     fstab.qcom \
@@ -400,7 +401,8 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.target.rc \
     ueventd.qcom.rc \
-    init.zenparts.rc
+    init.zenparts.rc \
+    init.spectrum.rc
 
 # Seccomp
 PRODUCT_COPY_FILES += \
